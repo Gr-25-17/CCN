f02928c1764c89581fd450b94d66ef8166e515d1
bellos92 <bellos.moshi@outlook.com>
2026-03-24 14:47:33 +0100

    skapade databasmodeller för Article, Category, Subscription & SubscriptionType

---
+++ NewsSite/Models/Entities/SubscriptionType.cs
@@ -0,0 +1,11 @@
+﻿using NewsSite.Models.Entities;
+namespace NewsSite.Models.Entities
+{
+    public class SubscriptionType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+
+}
