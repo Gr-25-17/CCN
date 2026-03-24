cf5ba5d56c1efd0b8b8383e9572cb986530f0f82
bellos92 <bellos.moshi@outlook.com>
2026-03-24 13:50:07 +0100

    Skapade ApplicationUser som implementerar IdentituUser och lägger till fler props. injectadeApplicationUser i ApplicationDbContext och registrerade i Program.cs. injectade ApplictionUser i _LoginPartial.cshtml

--- NewsSite/Program.cs
+++ NewsSite/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using NewsSite.Data;
+using NewsSite.Models.Entities;
 
 namespace NewsSite
 {
@@ -16,7 +17,9 @@ namespace NewsSite
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+
+            builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
 
