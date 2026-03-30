64f63863772fff8c79783e57194ccb2c336da5fc
JohanL900 <johan.liljeberg@lexlink.se>
2026-03-30 15:25:03 +0200

    test4

--- NewsSite/Models/APIs/WeatherForecast.cs
+++ NewsSite/Models/APIs/WeatherForecast.cs
@@ -9,7 +9,7 @@ namespace NewsSite.Models.APIs
         public string? City { get; set; }
         public string? Lang { get; set; }
         public int TemperatureC { get; set; }
-        public int TemperatureF { get; set; } //test
+        public int TemperatureF { get; set; } 
         public int Humidity { get; set; }
         public int WindSpeed { get; set; }
         public DateTime Date { get; set; }
