0a81eadd7d95153047d201d60aae073bc763b9af
JohanL900 <johan.liljeberg@lexlink.se>
2026-03-30 12:25:48 +0200

    Small fix for Weatherforcast, warnings found with git workflow. No reason to start new bracnh for this small fix

--- NewsSite/Models/APIs/WeatherForecast.cs
+++ NewsSite/Models/APIs/WeatherForecast.cs
@@ -5,22 +5,22 @@ namespace NewsSite.Models.APIs
     public class WeatherForecast
     {
         [JsonProperty("summary")]
-        public string CurrentWeather { get; set; } //Called "summary" before
-        public string City { get; set; }
-        public string Lang { get; set; }
+        public string? CurrentWeather { get; set; } //Called "summary" before
+        public string? City { get; set; }
+        public string? Lang { get; set; }
         public int TemperatureC { get; set; }
         public int TemperatureF { get; set; }
         public int Humidity { get; set; }
         public int WindSpeed { get; set; }
         public DateTime Date { get; set; }
         public int UnixTime { get; set; }
-        public Icon Icon { get; set; }
-    }
+        public Icon Icon { get; set; } = new Icon { Url = "", Code = "" };
+}
 
     public class Icon
     {
-        public string Url { get; set; }
-        public string Code { get; set; }
+        public string? Url { get; set; }
+        public string? Code { get; set; }
     }
 
 }
