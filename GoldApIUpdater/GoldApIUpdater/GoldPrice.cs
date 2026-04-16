8ab21ae7def1a38aee9ba84e2e90e9fd680755b3
JohanL900 <johan.liljeberg@lexlink.se>
2026-04-16 10:15:17 +0200

    gold first test

---
+++ GoldApIUpdater/GoldApIUpdater/GoldPrice.cs
@@ -0,0 +1,16 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+public class GoldPrice : ITableEntity
+{
+    // Mandatory for Azure Tables
+    public string PartitionKey { get; set; } = "Gold";
+    public string RowKey { get; set; }
+    public DateTimeOffset? Timestamp { get; set; }
+    public ETag ETag { get; set; } = ETag.All;
+
+    // Your custom data from the API
+    public float Close { get; set; }
+    public float PrevClose { get; set; }
+    public float PercentChange { get; set; }
+}
---
+++ GoldApIUpdater/GoldApIUpdater/StockPrice.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+
+    public class StockPrice
+    {
+        [JsonPropertyName("top10")]
+        public Top10[] Top10Stock { get; set; }
+    }
+
+
+    public class Top10
+    {
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        public float Close { get; set; }
+        public float PrevClose { get; set; }
+        public float PercentChange { get; set; }
+    }
+
+
