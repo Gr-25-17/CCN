7fb104eba1da599664e5329d9e1eb879e6ce4367
bellos92 <trorox1@gmail.com>
2026-05-13 14:17:06 +0200

    Implement default subscription type lookup

--- NewsSite/Repositories/Implementations/SubscriptionRepository.cs
+++ NewsSite/Repositories/Implementations/SubscriptionRepository.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using NewsSite.Data;
 using NewsSite.Models.Entities;
 using NewsSite.Repositories.Interfaces;
@@ -26,6 +26,21 @@ namespace NewsSite.Repositories.Implementations
                 .Where(s => s.UserId == userId)
                 .OrderByDescending(s => s.EndDate)
                 .FirstOrDefaultAsync();
+        }
+
+        public async Task<int> GetDefaultSubscriptionTypeIdAsync()
+        {
+            var subscriptionType = await _context.SubscriptionTypes
+                .AsNoTracking()
+                .OrderBy(x => x.Id)
+                .FirstOrDefaultAsync();
+
+            if (subscriptionType is null)
+            {
+                throw new InvalidOperationException("No subscription types exist in the database.");
+            }
+
+            return subscriptionType.Id;
         }
 
         public async Task SaveAsync(Subscription subscription)
