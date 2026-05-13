aa36b02cd49b0e50a60f14edac3db4f4dce3b25d
bellos92 <trorox1@gmail.com>
2026-05-13 12:52:48 +0200

    Add weekly newsletter service test skeleton

---
+++ NewsSite.Tests/Services/WeeklyNewsletterServiceTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace NewsSite.Tests.Services;
+
+public class WeeklyNewsletterServiceTests
+{
+    [Fact]
+    public void Placeholder()
+    {
+        Assert.True(true);
+    }
+}
