95b8ae4010eed2d18fb93e6090a6fd9b2bec4b96
bellos92 <trorox1@gmail.com>
2026-05-13 12:52:21 +0200

    Add article archive service test skeleton

---
+++ NewsSite.Tests/Services/ArticleArchiveServiceTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace NewsSite.Tests.Services;
+
+public class ArticleArchiveServiceTests
+{
+    [Fact]
+    public void Placeholder()
+    {
+        Assert.True(true);
+    }
+}
