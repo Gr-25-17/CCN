1aad6ae8c35414fb1327ca30f82a9e6503b2bd59
bellos92 <trorox1@gmail.com>
2026-05-13 14:26:14 +0200

    Add return URL to payment view model

--- NewsSite/Models/ViewModels/PaymentViewModel.cs
+++ NewsSite/Models/ViewModels/PaymentViewModel.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
 namespace NewsSite.Models.ViewModels
 {
@@ -6,22 +6,23 @@ namespace NewsSite.Models.ViewModels
     {
         [Required]
         [Display(Name = "Name of card owner")]
-        public string CardName { get; set; }
+        public required string CardName { get; set; }
 
         [Required]
         [Display(Name = "Card Number")]
         [RegularExpression(@"^\d{16}$", ErrorMessage = "Card number must be 16 digits.")]
-        public string CardNumber { get; set; }
+        public required string CardNumber { get; set; }
 
         [Required]
         [Display(Name = "Expiration Date")]
         [RegularExpression(@"^(0[1-9]|1[0-2])\/\d{2}$", ErrorMessage = "Expiry date must be in MM/YY format.")]
-        public string ExpirationDate { get; set; }
+        public required string ExpirationDate { get; set; }
 
         [Required]
         [Display(Name = "CVV")]
         [RegularExpression(@"^\d{3}$", ErrorMessage = "CVV must be 3 digits.")]
-        public string CVV { get; set; }
+        public required string CVV { get; set; }
 
+        public string? ReturnUrl { get; set; }
     }
 }
