356b7c790ae0efd8fa33730fa062d334f5518024
JT <johannes.tingberg@lexlink.se>
2026-05-21 12:00:07 +0200

    syntaxfel

--- NewsSite/ViewComponents/WeatherCardVC.cs
+++ NewsSite/ViewComponents/WeatherCardVC.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NewsSite.Mapping;
 using NewsSite.Services.Interfaces;
-using NewsSite.Models.ViewModels
+using NewsSite.Models.ViewModels;
 
 namespace NewsSite.ViewComponents;
 
@@ -12,7 +12,7 @@ public class WeatherCardVC(IWeatherService weatherService) : ViewComponent
         var weather = await weatherService.GetWeatherAsync(city);
 
         return detailed
-            ? View("Detailed", weather.ToWeatherViewModel() ?? new WeatherViewModel())
-            : View("Default", weather.ToViewModel() ?? new WeatherBasicVM());
+            ? View("Detailed", weather?.ToWeatherViewModel() ?? new WeatherViewModel())
+            : View("Default", weather?.ToViewModel() ?? new WeatherBasicVM());
     }
 }
